da6936b0c825ff209d7efa10016050ca64daf5bd
Greg T <greguus@gmail.com>
2023-09-17 16:52:47 +1000

    testing after crash

--- scan/views.py
+++ scan/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, you are at index in scan")
+    ip_list = ["10.10.30.101", "10.10.30.102", "10.10.30.103"]
+    context = {"ip_list": ip_list}
+    return render(request, "scan/index.html", context)
--- scanner/settings.py
+++ scanner/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files
+STATIC_URL = 'static/'
+STATIC_ROOT = '/home/gregrdp/network_storage/scanner/static'
